79f17307cdcb7fc439524906243bcaf2763560f6
Fred Morrison <fpmorrison@verizon.net>
2020-02-17 18:00:00 -0500

    fix formatting issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, VERSION, OnInit } from '@angular/core';
 import { of, from } from 'rxjs';
 
-
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
@@ -16,15 +15,15 @@ export class AppComponent implements OnInit {
     of(2, 4, 6, 8).subscribe(console.log);
 
     from([20, 15, 10, 5]).subscribe(
-          item => console.log(`resulting item .. ${item}`),
-          err => console.log(`${err}`),
-          () => console.log('complete')
-        );
+      item => console.log(`resulting item .. ${item}`),
+      err => console.log(`${err}`),
+      () => console.log('complete')
+    );
 
     of(...['Apple1', 'Apple2', 'Apple3']).subscribe(
-          apple => console.log(`Apple was emitted ${apple}`),
-          err => console.log(`${err}`),
-          () => console.log('No more apples.  Go home.')
-        );
+      apple => console.log(`Apple was emitted ${apple}`),
+      err => console.log(`${err}`),
+      () => console.log('No more apples.  Go home.')
+    );
   }
 }
