8fa6daa90c291ca33e4ad58bb20baed1c1ac635d
Fred Morrison <fpmorrison@verizon.net>
2020-02-17 09:30:34 -0500

    add routing options, Deborah Kurata's vscode settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 
 
 const routes: Routes = [];
+const routeOptions: ExtraOptions = {enableTracing: true};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routeOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
