4ee0c646f4f889849a6fcc34ce825e5d69ab62f6
Fred Morrison <fpmorrison@verizon.net>
2020-02-17 11:11:47 -0500

    add RxJS practice code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION, OnInit } from '@angular/core';
+import { of, from } from 'rxjs';
+
 
 @Component({
   selector: 'pm-root',
@@ -10,5 +12,19 @@ export class AppComponent implements OnInit {
   ngVersion: string;
   ngOnInit(): void {
     this.ngVersion = VERSION.full;
+
+    of(2, 4, 6, 8).subscribe(console.log);
+
+    from([20, 15, 10, 5]).subscribe(
+          item => console.log(`resulting item .. ${item}`),
+          err => console.log(`${err}`),
+          () => console.log('complete')
+        );
+
+    of(...['Apple1', 'Apple2', 'Apple3']).subscribe(
+          apple => console.log(`Apple was emitted ${apple}`),
+          err => console.log(`${err}`),
+          () => console.log('No more apples.  Go home.')
+        );
   }
 }
