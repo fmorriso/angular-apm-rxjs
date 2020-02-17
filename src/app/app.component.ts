025fd150558a83062ae0dae4430984be0c05b976
Fred Morrison <fpmorrison@verizon.net>
2020-02-17 10:52:13 -0500

    clean out boiler plate code, add Angular Version information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-apm-rxjs';
+  ngVersion: string;
+  ngOnInit(): void {
+    this.ngVersion = VERSION.full;
+  }
 }
