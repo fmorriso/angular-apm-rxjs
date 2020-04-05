022f2a2ec224231f929f9a0693f743c413e72585
Fred Morrison <fpmorrison@verizon.net>
2020-04-05 09:03:46 -0400

    hard-coded filter value

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 //
 import { Observable, EMPTY } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 //
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -9,19 +9,29 @@ import { ProductService } from './product.service';
 @Component({
 	templateUrl: './product-list.component.html',
 	styleUrls: ['./product-list.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductListComponent {
 	pageTitle = 'Product List';
 	errorMessage = '';
 	categories;
+	selectedCategoryId = 1;
 
-	products$ = this.productService.productsWithCategory$
-		.pipe(
-			catchError(err => {
-				this.errorMessage = err;
-				return EMPTY;
-			})
+	products$ = this.productService.productsWithCategory$.pipe(
+		catchError((err) => {
+			this.errorMessage = err;
+			return EMPTY;
+		})
+	);
+
+	productsSimpleFilter$ = this.productService.productsWithCategory$.pipe(
+		map((products) =>
+			products.filter((product) =>
+				this.selectedCategoryId
+					? product.categoryId === this.selectedCategoryId
+					: true
+			)
+		)
 	);
 
 	constructor(private productService: ProductService) {}
